f473d2ef96eac74ce7b44ba7abdb26eccc1c0b44
Tasik420 <86363853+Tasik420@users.noreply.github.com>
2022-03-27 17:33:20 +0600

    Update afoot.py

--- modules/afoot.py
+++ modules/afoot.py
@@ -11,4 +11,4 @@ async def runafoot(event):
     ridogramuserdetails = await event.get_sender()
     messagelocation = event.to_id
     ridogramimage = "https://media.discordapp.net/attachments/847160530191122482/943525406655524905/20220216_210354.jpg"
-    await client.send_file(messagelocation, ridogramimage, caption=f"Dear {ridogramuserdetails.first_name} {ridogramuserdetails.last_name},\nI'm glad to announce that Team Tasik is able to assist you.\n\n╭─⊸ Team Tasik Version: 0.2.0\n├─⊸ Python Version: {python_version()}\n╰─⊸ Telethon Version: {version.__version__}\n\nThank You,\nTeam Tasik")
+    await client.send_file(messagelocation, ridogramimage, caption=f"Dear {ridogramuserdetails.first_name} {ridogramuserdetails.last_name},\nI'm glad to announce that Team Tasik is able to assist you.\n\n╭─⊸ Team Tasik Version: 0.5.0\n├─⊸ Python Version: {python_version()}\n╰─⊸ Telethon Version: {version.__version__}\n\nThank You,\nTeam Tasik")
